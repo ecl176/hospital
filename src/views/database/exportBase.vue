ff1e89c2593029cd476d9a9d7eebb5e10eeb4ec3
apple <apple@zhugaoxiang.local>
2020-05-25 22:41:48 +0800

    update

--- src/views/database/exportBase.vue
+++ src/views/database/exportBase.vue
@@ -7,29 +7,91 @@
       <a-upload
         name="file"
         :multiple="true"
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        @change="handleChange"
+        :beforeUpload="beforeUpload"
+        :customRequest = 'customRequest'
+        accept="image/*, .xlsx, .xls"
       >
         <a-button type="primary">上传</a-button>
       </a-upload>
-      <a-button type="primary" style="margin-left: 20px;">导出</a-button>
+      <a-button type="primary" style="margin-left: 20px;" @click="hancleExport">导出</a-button>
     </div>
   </div>
 </template>
 <script>
+import { downloadFileFromResource } from '@/utils/file'
   export default {
     data() {
       return {
-        
+        allImageData:[],
+        fileData: [],
+        allFileNum: 0
       };
     },
     mounted() {
     },
     methods: {
-      
+      hancleExport() {
+        const self = this;
+        const params = {
+          "truncateDatabase": true
+        };
+        self.$http.post('/database/export', params, {
+          responseType: 'blob'
+        })
+        .then((res) => {
+          downloadFileFromResource(res)
+        }).catch(() => {
+          self.$message.error('请求失败');
+        });
+      },
+      beforeUpload(file) {
+        const formData = new FormData();
+        formData.append('file', file.file);
+        if(file.type.indexOf('image') !== -1) {
+          this.allImageData.push(formData);
+        } else {
+          this.fileData = [];
+          this.fileData.push(formData);
+        }
+      },
+      customRequest() {
+        const self = this;
+        self.allFileNum += 1;
+        if(self.allFileNum == self.allImageData.length + self.fileData.length) {
+          self.allFileNum = 0;
+          if(self.allImageData.length > 0) {
+            const params = self.allImageData;
+            self.$http.post('/database/import/image', params)
+            .then((res) => {
+              debugger;
+            }).catch(() => {
+              self.allImageData = [];
+              self.$message.error('上传失败');
+            });
+          }
+          if(self.fileData.length > 0) {
+            const params = self.fileData[0];
+            self.$http.post('/database/import/data', params)
+            .then((res) => {
+              debugger;
+            }).catch(() => {
+              self.fileData = [];
+              self.$message.error('上传失败');
+            });
+          }
+        }
+      },
+
     }
   };
 </script>
+<style lang="scss">
+  .export-box {
+    .ant-upload-list {
+      display: none !important;
+    }
+  }
+</style>
 <style lang="scss" scoped>
   .database-content {
     padding-bottom: 20px;
